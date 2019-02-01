0a9e48a2ee321591a68e22ba82d0cb7ce81bde39
Ark <konekred@gmail.com>
2019-02-01 14:33:40 +0800

    up

--- routes/searches.js
+++ routes/searches.js
@@ -1,7 +1,8 @@
 const root = process.cwd()
+var faker = require('faker')
 const router = require('express').Router()
 
-//---> /searches
+
 router.get('/', (req, res) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -11,5 +12,42 @@ router.get('/', (req, res) => {
     }
   })
 })
+
+
+router.get('/:id', (req, res) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      
+      const search = {
+        location: faker.address.country(),
+        keywords: `${faker.name.jobDescriptor()} ${faker.name.jobType()}`
+      }
+
+      let profiles = []
+      for (let i = 0; i < 100; i++) {
+        const firstName = faker.name.firstName()
+        const lastName = faker.name.lastName()
+        const fullName = `${firstName} ${lastName}`
+
+        profiles.push({
+          id: i + 1,
+          firstName: firstName,
+          lastName: lastName, 
+          fullName: fullName,
+          country: faker.address.country()
+        })
+      }
+
+      res.render('searches-view', {
+        search,
+        profiles
+      })
+
+    } catch (err) {
+      res.send('server error')
+    }
+  })
+})
+
 
 module.exports = router
